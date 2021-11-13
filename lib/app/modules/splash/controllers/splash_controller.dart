33b4d43a5cb3dbb4b7a21137b73864277cf8520e
Sanoj Raja <pimen07860@gmail.com>
2021-11-13 20:23:32 +0530

    Added More Permission for Location in Android.

--- lib/app/modules/splash/controllers/splash_controller.dart
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -1,15 +1,26 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+import 'package:location/location.dart';
 import 'package:sartia_global/app/constants/app_assest.dart';
 import 'package:sartia_global/app/routes/app_pages.dart';
 
 class SplashController extends GetxController {
   final logo = AppImages.logo;
   User? user = FirebaseAuth.instance.currentUser;
+  Location location = Location();
 
   @override
   void onInit() {
     super.onInit();
+    location.onLocationChanged.listen(
+      (currentLocation) {
+        sendLocationToFirestore(
+          currentLocation.longitude ?? 0.0,
+          currentLocation.longitude ?? 0.0,
+        );
+      },
+    );
   }
 
   @override
@@ -21,11 +32,22 @@ class SplashController extends GetxController {
           Get.offAndToNamed(Routes.HOME);
         } else
           Get.offAndToNamed(Routes.GOOGLE_LOGIN);
-          print('route');
       },
     );
   }
 
   @override
   void onClose() {}
+
+  Future sendLocationToFirestore(double latitude, double longitude) async {
+    Map<String, dynamic> userLocation = {
+      'Latitude': latitude,
+      'Longitude': longitude,
+    };
+
+    CollectionReference locationReference =
+        FirebaseFirestore.instance.collection('User_Location');
+
+    await locationReference.add(userLocation);
+  }
 }
