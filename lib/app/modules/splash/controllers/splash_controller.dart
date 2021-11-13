4c9d43a09ea5a05732495bfeb8b9d3e37288825c
Sanoj Raja <pimen07860@gmail.com>
2021-11-13 18:25:34 +0530

    Added & Created Splash Screen.

---
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:sartia_global/app/constants/app_assest.dart';
+import 'package:sartia_global/app/routes/app_pages.dart';
+
+class SplashController extends GetxController {
+  final logo = AppImages.logo;
+  User? user = FirebaseAuth.instance.currentUser;
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+    Future.delayed(Duration(seconds: 2)).then(
+      (_) {
+        if (user != null) {
+          Get.offAndToNamed(Routes.HOME);
+        } else
+          // Get.offAndToNamed(Routes.AUTH);
+          print('route');
+      },
+    );
+  }
+
+  @override
+  void onClose() {}
+}
---
+++ lib/app/modules/splash/views/splash_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/splash_controller.dart';
+
+class SplashView extends GetView<SplashController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            child: Image.asset(
+              controller.logo,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
